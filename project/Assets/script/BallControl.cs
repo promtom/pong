932acb3d4a8837529b287c40d5db2ab7223e6205
Aldi Mulia Wijaya <deadreaperco@gmail.com>
2021-09-07 18:16:50 +0700

    test1
    semoga gak ngebug

---
+++ project/Assets/script/BallControl.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+    // Titik asal lintasan bola saat ini
+    private Vector2 trajectoryOrigin;
+    
+    private int starting = 0;
+    Vector2 keeper; //mengingat velonvity awal
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        // Mulai game
+        trajectoryOrigin = transform.position;
+        RestartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ResetBall()
+    {
+        this.starting = 0;
+        // Reset posisi menjadi (0,0)
+        transform.position = Vector2.zero;
+
+        // Reset kecepatan menjadi (0,0)
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+        Vector2 newDirect;
+
+        //di push ball yang yrandom di hidden doang kodingnya jadiin comment, trus yang bawahnya hapus randomnya
+
+        //starting force
+        if (this.starting > 0) { 
+            float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+            // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
+            float randomDirection = Random.Range(0, 2);
+
+            // -1 = kiri 
+            if (randomDirection < 1.0f)
+                newDirect = new Vector2(-xInitialForce, yRandomInitialForce);
+            else
+                newDirect = new Vector2(xInitialForce, yRandomInitialForce);
+
+            //simpen nilai
+            keeper = newDirect;
+        }
+        //counter
+        else
+        {
+            float yDirection = Random.Range(-yInitialForce, yInitialForce);
+            float xDirection = Random.Range(-xInitialForce, xInitialForce);
+            Vector2 tmpDirect = new Vector2(xDirection, yDirection); // buat arah baru
+            Vector2 newVel = keeper.magnitude * tmpDirect.normalized; //kecepatan awal dengan arah baru
+            newDirect = new Vector2(newVel.x,newVel.y);
+        }
+        // Tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
+        rigidBody2D.AddForce(newDirect);
+    }   
+    void RestartGame()
+    {
+        // Kembalikan bola ke posisi semula
+        ResetBall();
+        // Setelah 2 detik, berikan gaya ke bola
+        Invoke("PushBall", 2);
+        this.starting++;
+    }
+    // Ketika bola beranjak dari sebuah tumbukan, rekam titik tumbukan tersebut
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+    // Untuk mengakses informasi titik asal lintasan
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+}
---
+++ project/Assets/script/PlayerControl.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    //control
+    public KeyCode upButton = KeyCode.W;
+    public KeyCode downButton = KeyCode.S;
+
+    //batasan
+    public float speed = 10.0f;
+    public float yBoundary = 9.0f;
+    private int score;
+
+    //2D raket
+    private Rigidbody2D rigidBody2D;
+    
+    // Titik tumbukan terakhir dengan bola, untuk menampilkan variabel-variabel fisika terkait tumbukan tersebut
+    private ContactPoint2D lastContactPoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Dapatkan kecepatan raket sekarang.
+        Vector2 velocity = rigidBody2D.velocity;
+        if (Input.GetKey(upButton)) velocity.y = speed; //atas
+        else if (Input.GetKey(downButton)) velocity.y = -speed; //kebawah
+        else velocity.y = 0.0f; //idle
+        // Masukkan kembali kecepatannya ke rigidBody2D.
+        rigidBody2D.velocity = velocity;
+        
+        // Dapatkan posisi raket sekarang.
+        Vector3 position = transform.position;  
+        if (position.y > yBoundary) position.y = yBoundary; //batas atas
+        else if (position.y < -yBoundary) position.y = -yBoundary; //batas bawah
+        // Masukkan kembali posisinya ke transform.
+        transform.position = position;
+    }
+
+    // Menaikkan skor sebanyak 1 poin
+    public void IncrementScore()
+    {
+        score++;
+    }
+
+    // Mengembalikan skor menjadi 0
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    // Mendapatkan nilai skor
+    public int Score
+    {
+        get { return score; }
+    }
+
+    // Untuk mengakses informasi titik kontak dari kelas lain
+    public ContactPoint2D LastContactPoint
+    {
+        get { return lastContactPoint; }
+    }
+
+    // Ketika terjadi tumbukan dengan bola, rekam titik kontaknya.
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Ball"))
+        {
+            lastContactPoint = collision.GetContact(0);
+        }
+    }
+
+}
